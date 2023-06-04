8125c6018455ab2e10853fa3d476a22778837057
Blink29 <purukumar2905@gmail.com>
2023-06-04 13:56:16 +0530

    .

--- craweler/scrape.py
+++ craweler/scrape.py
@@ -1,42 +1,49 @@
 from bs4 import BeautifulSoup
 import requests
 
-# source = requests.get('https://gogoanime.cl/category/kimetsu-no-yaiba').text
+def get_anime_links(page_no):
+    source_list = requests.get(f"https://gogoanime.cl/anime-list.html?page={page_no}").text
+    soup_anime_list = BeautifulSoup(source_list, 'lxml')
+    anime_list = soup_anime_list.find('div', class_="anime_list_body")
 
-source_list = requests.get("https://gogoanime.cl/anime-list.html").text
+    links= []
+    for anime_links in anime_list.find_all('a'):
+        anime_link = anime_links['href']
+        link = f"https://gogoanime.cl{anime_link}"
+        links.append(link)
+    
+    return links
 
-soup_anime_list = BeautifulSoup(source_list, 'lxml')
+def get_anime_id_from_anime_link(anime_link):
+    source  = requests.get(anime_link).text
+    soup = BeautifulSoup(source, 'lxml')
+    anime_id = soup.find('input', class_='movie_id')['value']
+    return anime_id
 
-anime_list = soup_anime_list.find('div', class_="anime_list_body")
-for anime_links in anime_list.find_all('a'):
-    anime_link = anime_links['href']
-    # print(anime_link)
+def get_episode_num(anime_link):
+    source = requests.get(anime_link).text
+    soup = BeautifulSoup(source, 'lxml')
+    ep_num = soup.find('a', class_='active')['ep_end']
+    return ep_num
 
-    source_anime_name = requests.get(f"https://gogoanime.cl{anime_link}").text
-    soup_anime_name = BeautifulSoup(source_anime_name, 'lxml')
-    ep_num = soup_anime_name.find('a', class_='active')['ep_end']
-    # print(ep_num)
+def get_episode_links(anime_link):
+    anime_id= get_anime_id_from_anime_link(anime_link)
+    ep_num = get_episode_num(anime_link)
+    endpoint = f"https://ajax.gogo-load.com/ajax/load-list-episode?ep_start=0&ep_end={ep_num}&id={anime_id}"
+    source_code = requests.get(endpoint)
+    soup = BeautifulSoup(source_code, 'lxml')
+    episodes = [ "https://gogoanime.cl" + element["href"].strip() for element in soup.find_all('a')]
+    return episodes
 
-    anime_name = anime_link.split("/")[2]
-    # print(anime_name)
+def get_anime_name(anime_link):
+    hiphen_name = anime_link.split("/")[-1]
+    anime_name = hiphen_name.replace("-", " ").title()
+    return anime_name
 
-    for i in range(1, int(ep_num)):
-        source_anime_link = requests.get(f"https://gogoanime.cl/{anime_name}-episode-{i}").text
-        soup_anime_link = BeautifulSoup(source_anime_link, 'lxml')
-        iframe = soup_anime_link.find('iframe')['src']
-        vid_src = iframe
-        print(soup_anime_link.find('title').text)
-        print(vid_src)
-
-
-# ep_num = soup.find('a', class_='active')['ep_end']
-# print(ep_num)
-
-# source = requests.get("https://www3.gogoanimes.fi/kimetsu-no-yaiba-episode-1").text
-
-# soup = BeautifulSoup(source, 'lxml')
-
-# iframe = soup.find('iframe')
-# vid_src = iframe['src']
-# print(vid_src)
+def extract_iframe_from_episode(episode_link):
+    source = requests.get(episode_link)
+    soup = BeautifulSoup(source.text, 'lxml')
+    iframe = soup.find('iframe')
+    vid_src = iframe["src"]
+    return vid_src
 
