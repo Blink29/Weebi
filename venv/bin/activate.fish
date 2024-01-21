2c3af00f467b5a2f36a79f35df43123dbdda52df
Blink29 <purukumar2905@gmail.com>
2024-01-21 16:56:29 +0530

    scaper progess

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/paurushkumar/Desktop/Project Weebi/venv"
+set -gx VIRTUAL_ENV "/Users/paurushkumar/Desktop/Weebi/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
